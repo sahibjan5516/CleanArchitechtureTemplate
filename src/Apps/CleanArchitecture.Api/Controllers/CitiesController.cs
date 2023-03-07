6328054ba3afc5c8219f1661f7d7c508502a0d72
Sahib Jan <sahibjan5516@gmail.com>
2023-03-07 10:30:16 +0500

    Initialize refactor/IdentityServer-NSwag

---
+++ src/Apps/CleanArchitecture.Api/Controllers/CitiesController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Cities.Commands.Create;
+using CleanArchitecture.Application.Cities.Commands.Delete;
+using CleanArchitecture.Application.Cities.Commands.Update;
+using CleanArchitecture.Application.Cities.Queries.GetCities;
+using CleanArchitecture.Application.Cities.Queries.GetCityById;
+using CleanArchitecture.Application.Common.Models;
+using CleanArchitecture.Application.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Api.Controllers
+{
+    [Authorize]
+    public class CitiesController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<ServiceResult<List<CityDto>>>> GetAllCities(CancellationToken cancellationToken)
+        {
+            //Cancellation token example.
+            return Ok(await Mediator.Send(new GetAllCitiesQuery(), cancellationToken));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResult<CityDto>>> GetCityById(int id, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new GetCityByIdQuery { CityId = id }, cancellationToken));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResult<CityDto>>> Create(CreateCityCommand command, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(command, cancellationToken));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResult<CityDto>>> Update(UpdateCityCommand command, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(command, cancellationToken));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResult<CityDto>>> Delete(int id, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new DeleteCityCommand { Id = id }, cancellationToken));
+        }
+    }
+}
---
+++ src/Apps/CleanArchitecture.Api/Controllers/DistrictsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Models;
+using CleanArchitecture.Application.Districts.Commands.Create;
+using CleanArchitecture.Application.Districts.Queries;
+using CleanArchitecture.Application.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Api.Controllers
+{
+    [Authorize]
+    public class DistrictsController: BaseApiController
+    {
+        [HttpGet("{id}")]
+        public async Task<FileResult> Get(int id, CancellationToken cancellationToken)
+        {
+            var vm = await Mediator.Send(new ExportDistrictsQuery { CityId = id }, cancellationToken);
+
+            return File(vm.Content, vm.ContentType, vm.FileName);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResult<DistrictDto>>> Create(CreateDistrictCommand command, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(command, cancellationToken));
+        }
+    }
+}
